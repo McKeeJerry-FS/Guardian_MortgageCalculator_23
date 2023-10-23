e75ecafe19b13af217d328832f1bb76bffdb07af
Jerry McKee Jr <JerryMcKeeJr.Dev@gmail.com>
2023-10-23 15:06:30 -0400

    created first calculation functions

--- js/site.js
+++ js/site.js
@@ -1,12 +1,43 @@
-function displayMessage() {
-    // get the text out of input and assign to a variable
-    let msg = document.getElementById('message').value;
-    
-    Swal.fire(
-        {
-            backdrop: false,
-            title: 'Mortgage Calculator',
-            text: msg, // <-- replace this with the variable
-        }
-    );
+// get the values from the inputs
+
+
+function getValues(clickedBtn){
+    let principle = document.getElementById('loanAmount').value;
+    let principleNum = parseInt(principle);
+    let term = document.getElementById('term').value;
+    let termNum = parseInt(term);
+    let rate = document.getElementById('interestRate').value;
+    let rateFloat = parseFloat(rate);
+
+    let values = {
+        princ : principleNum,
+        termN : termNum,
+        rateN : rateFloat
+    }
+    return values;
 }
+
+function calculateRates(){
+    let values = getValues();
+    let totalPrinciple = document.getElementById('principle');
+    totalPrinciple.textContent = `Total Principle: \$${values.princ.toFixed(2).toLocaleString()}`;
+    
+    let totalInterestView = document.getElementById('totInterest');
+    let totalInt = values.princ * values.rateN;
+    totalInterestView.innerText = `Total Interest: \$${totalInt.toFixed(2).toLocaleString()}`;
+
+    let totalCostView = document.getElementById('totCost');
+    let totalCost = values.princ + totalInt;
+    totalCostView.innerText = `Toatl Cost: \$${totalCost.toFixed(2).toLocaleString()}`;
+
+    let monthlyPmtView = document.getElementById('monthlyPmt');
+    let monthPmt = totalCost / values.termN;
+    monthlyPmtView.innerText = `\$${monthPmt.toFixed(2).toLocaleString()}`;
+
+}
+
+
+
+// display the total values in the stats column
+
+// display the values in the table per each month
